10b56d0affc057f8401d06457ec3363856e00e81
daisies7779 <41288557+daisies7779@users.noreply.github.com>
2020-11-19 17:50:35 +0200

    Create lekcijasKonspekts.c

---
+++ lekcijasKonspekts.c
@@ -0,0 +1,7 @@
+//Kods atbilst lekcijas konspektam
+// https://www.youtube.com/watch?v=7GkFkI_rw6M&feature=youtu.be
+
+//definē ieeju/izeju kur 0 ir ieeja, bet 1 izeja, degs nultā un trešā diode
+P1DIR = 0b00001001;
+//reģists atbild par diodes iededzināšanu (izejas stāvokli), degs tikai nultā diode
+P1OUT = 0b00000001;
