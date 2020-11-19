84a82d1658e0f6b05a124ef65fc530b46381a98c
daisies7779 <41288557+daisies7779@users.noreply.github.com>
2020-11-19 16:05:34 +0200

    Create Hello_world.ino

---
+++ Hello_world.ino
@@ -0,0 +1,52 @@
+#include <msp430.h>
+int main()
+{
+  WDTCTL = WDTPW + WDTHOLD;
+  
+ // 1.4.aktivitāte 
+ //volatile unsigned int i=0;
+ 
+ //Visus 8 pinus pasludinam par izejām
+  P1DIR |= 0b11111111; 
+ //pasludinam, ka visi pini tagad nav ieslēgti
+  P1OUT &= ~0xFF; 
+ 
+ for( ; ; )
+ {
+   //1.1.aktivitāte
+   P1OUT |= 0b00000001; 
+   
+   //1.2.aktivitāte
+   //iededzināt LED, kas pieslēgts P1.1.
+   P1OUT |= 0b00000010;
+   
+   //1.3.aktivitāte
+   // Iededzināt P1.1. un P1.0
+   P1OUT |= 0b00000011; 
+   
+   
+   //1.4.aktivitāte
+  
+   /*
+   for(i=30000;i>1;i--);//aizture
+   //pēc aiztures izslēdzam 1 LED
+   P1OUT &= ~00000001; 
+   
+   for(i = 30000; i>1;i--); 
+   
+   //nokompilēt un ielādēt kontrolierī
+   
+  //kompaktākā versija
+  // P1OUT ^= 0b00000001;
+  // for(i = 30000; i>1;i--);   
+
+   */
+   
+ }
+}
+
+
+
+//9lapaspuses uzdevumam kods
+ P2SEL &= ~BIT7; //to use 2.7, 2.6 pins
+ P2SEL &= ~BIT6; //to use 2.7, 2.6 pins
