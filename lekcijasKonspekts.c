667936380a08bad6cbba3dfaf2d667ab85991ec4
daisies7779 <41288557+daisies7779@users.noreply.github.com>
2020-11-19 18:48:06 +0200

    Update lekcijasKonspekts.c

--- lekcijasKonspekts.c
+++ lekcijasKonspekts.c
@@ -5,3 +5,21 @@
 P1DIR = 0b00001001;
 //reģists atbild par diodes iededzināšanu (izejas stāvokli), degs tikai nultā diode
 P1OUT = 0b00000001;
+
+
+#include <msp430.h>
+int main()
+{
+  WDTCTL = WDTPW + WDTHOLD;
+  
+  //pasludinam par izeju P1.0 un P1.6
+  //bināri
+  //P1DIR |= 0b01000001; 
+  //heksadecimāli
+  //P1DIR |= 0x41; 
+  //ar konstanti
+  P1DIR |= (BIT6+BIT0); 
+  
+  
+
+}
