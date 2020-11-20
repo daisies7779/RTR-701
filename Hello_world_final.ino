60347113d7340e788bb77e27e47535410ed90e21
daisies7779 <41288557+daisies7779@users.noreply.github.com>
2020-11-20 16:14:06 +0200

    Update Hello_world_final.ino

--- Hello_world_final.ino
+++ Hello_world_final.ino
@@ -1,4 +1,3 @@
-//Pedeja versija kura bija iesniegta laboratorija
 #include <msp430.h>
 int main()
 {
@@ -45,8 +44,8 @@ int main()
     for(i = 30000; i>1;i--);   
   
    
-   //1.5aktivitate
-   
+   //1.5aktivitate skat modificéto versiju 1.6.
+   /*
    if ((P2IN&0b00001000) == 0b00001000)
  {
   P1OUT |= 0b10000000;//ieslēgt 7.led
@@ -55,6 +54,40 @@ int main()
  else
  {
   P1OUT &= ~0b10000000;//izslēgt 7.led
+ } 
+ */
+
+ //1.6.aktivitate
+ //POGA nav nospiesta, LED7 un LED6 spid
+ //POGA ir nospiesta LED7 un LED 6 nespid
+ /*
+ for(i=30000;i>1;i--);//aizture
+    if ((P2IN&0b00001000) == 0b00001000)
+ {
+  P1OUT &= ~0b11000000;//izsleg 6. un 7.led
+
+ }
+ else
+ {
+  P1OUT |= 0b11000000;//ieslegt 6. un 7.led
+ } 
+*/
+
+ //1.7.aktivitate
+ //POGA nav nospiesta, LED7 un LED6 spid
+ //POGA ir nospiesta LED7 un LED 6 nespid
+ 
+ for(i=30000;i>1;i--);//aizture
+    if ((P2IN&0b00001000) == 0b00001000)
+ {
+  P1OUT &= ~0b11000000;//izsleg 6. un 7.led
+  P1OUT |= 0b00100000; //iesledz 5.lde
+
+ }
+ else
+ {
+  P1OUT |= 0b11000000;//ieslegt 6. un 7.led
+  P1OUT &= ~0b00100000;//izsleg 5.led
  } 
    
    
